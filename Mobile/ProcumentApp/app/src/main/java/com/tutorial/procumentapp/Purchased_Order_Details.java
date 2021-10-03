fc75aa5baad5651922d1b251537c3c5d85734d79
kalpana65 <it19195230@my.sliit.lk>
2021-10-04 00:28:27 +0530

    Purchased_Order_Details implementation

---
+++ Mobile/ProcumentApp/app/src/main/java/com/tutorial/procumentapp/Purchased_Order_Details.java
@@ -0,0 +1,14 @@
+package com.tutorial.procumentapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Purchased_Order_Details extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_purchased_order_details);
+    }
+}

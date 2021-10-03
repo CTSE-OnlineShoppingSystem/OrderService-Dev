724ce2ef7eba117b04d997b27697a7a984aafa00
kalpana65 <it19195230@my.sliit.lk>
2021-10-03 23:24:17 +0530

    avilabilty page intial ui

---
+++ Mobile/ProcumentApp/app/src/main/java/com/tutorial/procumentapp/Availabile_Product.java
@@ -0,0 +1,14 @@
+package com.tutorial.procumentapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Availabile_Product extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_availabile_product);
+    }
+}

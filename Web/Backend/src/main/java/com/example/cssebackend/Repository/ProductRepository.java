f04a8b8dd6828698bce8e9bbda270fcd5ffdcda0
MininduKothalawala <66974099+MininduKothalawala@users.noreply.github.com>
2022-05-14 13:16:21 +0530

    Delete Web directory

--- Web/Backend/src/main/java/com/example/cssebackend/Repository/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.cssebackend.Repository;
-
-import com.example.cssebackend.Model.Product;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface ProductRepository extends MongoRepository<Product, String> {
-}
